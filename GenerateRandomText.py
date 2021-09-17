3d3f4656fca24dc86706f949dcd0e6d305d3f2eb
ImAFookingScarecrow <51989378+ImAFookingScarecrow@users.noreply.github.com>
2021-09-17 19:29:40 +0100

    added some files and code and shit

---
+++ GenerateRandomText.py
@@ -0,0 +1,14 @@
+import random
+
+numbers = 100
+digits = 20
+
+open('randomtext.txt', 'w').close() # clears the file
+
+filee = open("randomtext.txt", "a")
+
+for i in range(0, numbers):
+    string = ''
+    for i in range(0, digits):
+        string = string + str(random.randint(0, 1))
+    filee.write(string + '\n')
